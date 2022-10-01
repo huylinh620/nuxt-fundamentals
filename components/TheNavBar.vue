10eb5fb28115d95761d7d4dbc4473318447b58ea
huylinh620 <huylinh620@gmail.com>
2022-10-01 14:27:55 +0700

    Add source code

---
+++ components/TheNavBar.vue
@@ -0,0 +1,8 @@
+<template>
+  <nav class="nav">
+    <nuxt-link :to="{name: 'index'}" class="logo text-lg">
+        Nuxt Fundamentals
+    </nuxt-link>
+    <span>Nuxt Fundamentals</span>
+  </nav>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <TheNavBar/>
+    <nuxt/>
+  </div>
+</template>
+
+
+<script>
+import TheNavBar from '@/components/TheNavBar'
+
+export default {
+  components: {
+    TheNavBar
+  }
+}
+</script>
+
+<style>
+  .button-green {
+    display: inline-block;
+    border-radius: 4px;
+    border: 1px solid #3b8070;
+    color: #3b8070;
+    text-decoration: none;
+    padding: 10px 30px;
+  }
+
+  .button-green:hover {
+    color: #fff;
+    background-color: #3b8070;
+  }
+</style>
